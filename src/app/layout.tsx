8ea9775a299cae8dcc2adb4492e8a6d403df28fd
shalom <castroshalom77@gmail.com>
2026-03-25 00:42:17 +0800

    layout hydration warning corrected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} 
     >
       <body className="min-h-full flex flex-col">
         <ThemeProvider
